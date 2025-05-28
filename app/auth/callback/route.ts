275dc5e98188e0bda27d95530cc4019e6b3acd61
Winston Bandong <wband@Winstons-MacBook-Pro.local>
2025-05-27 22:16:16 -1000

    fixing google auth flow

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,15 +1,32 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { createServerClient } from '@supabase/ssr';
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
   const next = requestUrl.searchParams.get('next') ?? '/'; // Default redirect to home if 'next' is not present
+  const cookieStore = cookies();
 
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value;
+          },
+          set(name: string, value: string, options: any) {
+            cookieStore.set({ name, value, ...options });
+          },
+          remove(name: string, options: any) {
+            cookieStore.set({ name, value: '', ...options });
+          },
+        },
+      }
+    );
     try {
       await supabase.auth.exchangeCodeForSession(code);
       // URL to redirect to after sign in process completes
